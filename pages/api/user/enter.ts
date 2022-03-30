90e120748e1ec568c6b8ea8e1204dbb7ae31b0d2
n0eyes <doll4586633@naver.com>
2022-03-31 02:58:02 +0900

    feat: Add sending SMS and sending email

--- pages/api/user/enter.ts
+++ pages/api/user/enter.ts
@@ -2,14 +2,18 @@ import client from "libs/server/client";
 import withHandler from "libs/server/withHandler";
 import { ResponseType } from "libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import twilio from "twilio";
+import mail from "@sendgrid/mail";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
+  mail.setApiKey(process.env.SENDGRID_API_KEY!);
   const { email, phone } = req.body;
   const user = email ? { email } : { phone: +phone };
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
   const token = await client.token.create({
     data: {
@@ -28,6 +32,21 @@ async function handler(
     },
   });
 
+  if (phone) {
+    const result = await twilioClient.messages.create({
+      messagingServiceSid: process.env.MESSAGE_SID,
+      to: process.env.MY_PHONE!,
+      body: payload,
+    });
+  } else if (email) {
+    const email = await mail.send({
+      from: "doll4586633@naver.com",
+      to: "doll4586633@naver.com",
+      subject: "Carrot Market Verification",
+      text: payload,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+  }
   // user = await client.user.findUnique({
   //   where: {
   //     ...user,
